e485ca629cdd14d3759e1764e30c4b1d3cc98301
Muhammet Cin <muhammetacin@gmail.com>
2022-01-12 11:00:03 +0100

    Create for loop to iterate every item in ITEMS_LIST

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
  */
 
 // TODO: import ITEMS_LIST from collection.js
-import { ITEMS_LIST } from "./collection.js";
+import { ITEMS_LIST as item } from "./collection.js";
 
 // ITEMS_LIST.forEach((dragonItem) => {
 //     console.log(dragonItem.name);
@@ -25,19 +25,15 @@ section.appendChild(headingH3Items);
 
 // TODO: create div with classes (container div for cards) and add to section, only one is needed for the page
 let containerDiv = document.createElement("div");
-containerDiv.classList.add(
-  "row",
-  "d-flex",
-  "flex-column",
-  "flex-md-row",
-  "flex-lg-row",
-  "row-cols-md-2",
-  "row-cols-lg-4",
-  "m-auto",
-  "g-3"
-);
+containerDiv.classList.add("row", "d-flex", "flex-column", "flex-md-row", "flex-lg-row", "row-cols-md-2", "row-cols-lg-4", "m-auto", "g-3");
 section.appendChild(containerDiv);
 // console.log(containerDiv);
+
+
+// ALL THE CODE BELOW IS NECESSARY FOR EVERY CARD SO 
+// PUT THESE IN A FOR LOOP TO CREATE A CARD FOR EVERY ITEM IN THE COLLECTION DYNAMICALLY
+
+item.forEach((item) => {
 
 // TODO: create cardContainer with classes (necessary for layout of the card elements) and add to containerDiv, for every card needed
 let cardContainer = document.createElement("div");
@@ -59,8 +55,8 @@ card.appendChild(cardImageContainer);
 
 // TODO: create cardImage in cardImageContainer with src, alt and class, for every card needed
 let cardImage = document.createElement("img");
-cardImage.src = ITEMS_LIST[2].image;
-cardImage.alt = ITEMS_LIST[2].examine;
+cardImage.src = item.image;
+cardImage.alt = item.examine;
 cardImage.classList.add("card-img-top");
 cardImageContainer.appendChild(cardImage);
 // console.log(cardImage);
@@ -74,24 +70,24 @@ card.appendChild(cardBody);
 // TODO: create h5 heading, span badges in cardBody with classes, for every card needed
 let cardHeadingH5 = document.createElement("h5");
 cardHeadingH5.classList.add("card-title");
-cardHeadingH5.appendChild(document.createTextNode(ITEMS_LIST[2].name));
+cardHeadingH5.appendChild(document.createTextNode(item.name));
 cardBody.appendChild(cardHeadingH5);
 // console.log(cardHeadingH5);
 
 let cardSpanFirst = document.createElement("span");
 cardSpanFirst.classList.add("badge", "mb-3");
-if (ITEMS_LIST[2].type == "Weapon") {
-  cardSpanFirst.classList.add("bg-warning");
+if (item.type == "Weapon") {
+    cardSpanFirst.classList.add("bg-warning");
 } else {
-  cardSpanFirst.classList.add("bg-danger");
+    cardSpanFirst.classList.add("bg-danger");
 }
-cardSpanFirst.appendChild(document.createTextNode(ITEMS_LIST[2].type));
+cardSpanFirst.appendChild(document.createTextNode(item.type));
 cardBody.appendChild(cardSpanFirst);
 // console.log(cardSpanFirst);
 
 let cardSpanSecond = document.createElement("span");
 cardSpanSecond.classList.add("badge", "mb-3", "bg-success");
-if(ITEMS_LIST[2].tradeable) {
+if(item.tradeable) {
     cardSpanSecond.appendChild(document.createTextNode("Tradeable"));
 }
 cardBody.appendChild(cardSpanSecond);
@@ -100,7 +96,7 @@ cardBody.appendChild(cardSpanSecond);
 // TODO: create paragraph p and button with classes, for every card needed
 let cardText = document.createElement("p");
 cardText.classList.add("card-text");
-cardText.appendChild(document.createTextNode(ITEMS_LIST[2].description));
+cardText.appendChild(document.createTextNode(item.description));
 cardBody.appendChild(cardText);
 // console.log(cardText);
 
@@ -110,7 +106,9 @@ cardBody.appendChild(cardButtonContainer);
 
 let cardButton = document.createElement("a");
 cardButton.classList.add("btn", "btn-primary");
-cardButton.appendChild(document.createTextNode("Buy " + ITEMS_LIST[2].name));
+cardButton.appendChild(document.createTextNode("Buy " + item.name));
 cardButton.href = "#";
 cardButtonContainer.appendChild(cardButton);
 // console.log(cardButton);
+
+});
