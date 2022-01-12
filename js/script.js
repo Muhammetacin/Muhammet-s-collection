04d8b3846087ab0600857abd0fb02799ca3166b7
Muhammet Cin <muhammetacin@gmail.com>
2022-01-12 09:44:54 +0100

    Grab section from html + create h3 heading and append as a child to section

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
+// TODO: import ITEMS_LIST from collection.js
 import { ITEMS_LIST } from "./collection.js";
 
 // ITEMS_LIST.forEach((dragonItem) => {
 //     console.log(dragonItem);
 // })
 
-console.log(ITEMS_LIST);
+// console.log(ITEMS_LIST);
+
+// TODO: select section to append new children (cards)
+const section = document.querySelector("section");
+
+// TODO: create h3 heading title "Items" and add it to the top of section (as a first child)
+let headingH3Items = document.createElement("h3")
+headingH3Items.appendChild(document.createTextNode("Items"));
+section.appendChild(headingH3Items);
+// console.log(headingH3Items);
 
